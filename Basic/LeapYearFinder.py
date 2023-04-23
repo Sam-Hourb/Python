03043fc9c944a21c069450fdc27f9077a5f35e0e
Sam-Hourb <121263083+Sam-Hourb@users.noreply.github.com>
2023-04-23 11:27:33 -0700

    Create LeapYearFinder.py

---
+++ Basic/LeapYearFinder.py
@@ -0,0 +1,13 @@
+Year = int(input("Which year do you want to check? "))
+
+if Year % 4 == 0:
+  if Year % 100 == 0:
+    if Year % 400 == 0:
+      print("Leap Year.")
+    else:
+      print("Not a Leap Year.") 
+  else:
+    print("Leap Year.")
+  
+else:
+  print("Not a Leap Year.")
